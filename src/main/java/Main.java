6220a1d63e1b4d1412c7338377d4b767ee86f575
Urs Reupke <ursreupke@gmail.com>
2014-04-11 14:52:14 +0200

    Cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.sun.javafx.collections.ImmutableObservableList;
+import com.sun.javafx.collections.ObservableListWrapper;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -5,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
 import javafx.stage.Stage;
 import net.miginfocom.layout.CC;
 import net.miginfocom.layout.LC;
@@ -19,57 +20,49 @@ public class Main extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    configureScene(stage);
-    stage.show();
+    Scene scene = createScene();
+    stage.setScene(scene);
+    showStage(stage);
   }
 
-  private void configureScene(Stage stage) {
-    MigPane parent = new MigPane(new LC().fill().insets("0").wrapAfter(2));
+  private Scene createScene() {
     final MigPane container = new MigPane(new LC().fill().insets("0").wrapAfter(1));
-
     Button control = new Button("Show Content");
-    parent.add(control);
-    parent.add(container);
-    
     control.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent actionEvent) {
         showContent(container);
       }
     });
-    
-    Scene scene = new Scene(parent);
-    stage.setScene(scene);
-    stage.setHeight(400);
-    stage.setWidth(800);
+    MigPane parent = new MigPane(new LC().fill().insets("0").wrapAfter(2));
+    parent.add(control);
+    parent.add(container);
+    return new Scene(parent);
   }
 
   private void showContent(MigPane container) {
     container.getChildren().clear();
-    MigPane titlePane = createTitle();
-    container.add(titlePane, new CC().pushX().growX());
     MigPane content = createContent();
     container.add(content, new CC().push().grow());
   }
-  
 
   private MigPane createContent() {
     MigPane content = new MigPane(new LC().insets("0").wrapAfter(2));
     MigPane view = new MigPane(new LC().insets("0"));
     ComboBox comboBox = new ComboBox<>();
-    Label label = new Label("Look!");
+    comboBox.setItems(new ImmutableObservableList("There is a label to my left!"));
+    
+    Label label = new Label("I should be visible!");
     view.add(label);
     view.add(comboBox);
     content.add(view, new CC().growX().pushX());
     return content;
   }
 
-  private MigPane createTitle() {
-    MigPane titlePane = new MigPane(new LC().fill().insets("0"));
-    Label title = new Label("Test");
-    title.setStyle("-fx-font-weight: bold");
-    titlePane.add(title);
-    titlePane.add(new Separator(), new CC().pushX().growX());
-    return titlePane;
+
+  private void showStage(Stage stage) {
+    stage.setHeight(400);
+    stage.setWidth(800);
+    stage.show();
   }
 }
